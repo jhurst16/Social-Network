8f19e9c43c9867b67cf0d81982856da91f001722
jhurst16 <jeremyhurst16@gmail.com>
2021-08-21 14:21:00 -0400

    initial setup for social media backend using MongoDB

---
+++ Models/Thought.js
@@ -0,0 +1,68 @@
+const { Schema, model, Types } = require('mongoose');
+const luxon = require('luxon');
+const { DateTime } = require('luxon');
+const dt = DateTime.now().toLocaleString(DateTime.DATETIME_HUGE);
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: dt,
+            get: () => dt
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+)
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: ()=> new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: dt,
+            get: () => dt
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
---
+++ server.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+mongoose.connect('mongodb://localhost/Social-Media-API', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`connected to localhost on port ${PORT}`));
