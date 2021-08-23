0dee9a2dcb173ceff96c39ca697f3b6431b8d508
jhurst16 <jeremyhurst16@gmail.com>
2021-08-22 23:55:05 -0400

    Ffurther structured files

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-const mongoose = require('mongoose');
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(require('./routes'));
+app.use(require('./routes/api'));
 
-mongoose.connect('mongodb://localhost/Social-Media-API', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/the-social-network', {
     useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
+//this logs and executes the mongo queries
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log(`connected to localhost on port ${PORT}`));
+app.listen(PORT, () => console.log(`Connected on port: ${PORT}`));
